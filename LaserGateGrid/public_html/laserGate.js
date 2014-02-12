19fb78e1bcaf83dffd46186a21a17f3177d64d50
Ashley <ablondewell@gmail.com>
2014-02-11 17:10:01 -0800

    Removed unneeded swipe functionality

--- LaserGateGrid/public_html/laserGate.js
+++ LaserGateGrid/public_html/laserGate.js
@@ -54,6 +54,7 @@ function level1() {
     $("#00").text("laser").addClass("laser");
     $("#60").text("laser").addClass("laser");
     $("#3" + numCols + "").text("laser").addClass("laser");
+    $("#" + numRows + "10").text("laser").addClass("laser")
     avatar = numRows.toString() + Math.floor(numCols/2).toString();
     $("#" + avatar + "").addClass("avatar");
 
@@ -84,50 +85,3 @@ for (i = 0; i <= numRows; i++) {
         };
     }
 }
-
-$(function() {
-
-//SWIPING FUNCTIONALITY FOR AVATAR
-//    //Enable swiping...
-//    $("table").swipe({
-//        //Generic swipe handler for all directions
-//        swipe: function(event, direction, distance, duration, fingerCount) {
-////            $('#grid').text("You swiped " + direction + " for " + distance + " pixels");
-//            var start = $(".avatar").attr("id");
-//            var moveSquares = Math.floor(distance / 75);
-//            var end = parseInt(start, 10);
-//            row = Math.floor(end / 10);
-//            col = end % 10;
-////            $("h1").text("row " + row + " col " + col + " " + Math.floor(end / 10) + "   " + end % 10 + "  " + end);
-//
-//            var end = parseInt(start, 10);
-//            if ($("#" + start + "").hasClass('right') && direction === 'right'.toString()) {
-//                col = (moveSquares + col > numCols) ? numCols.toString() : (col + moveSquares);
-//            }
-//            else if ($("#" + start + "").hasClass('left') && direction === 'left'.toString()) {
-//                col = (col - moveSquares < 0) ? '0' : (col - moveSquares);
-//            }
-//            else if ($("#" + start + "").hasClass('up') && direction === 'up'.toString()) {
-//                row = (row - moveSquares < 0) ? '0' : (row - moveSquares);
-//            }
-//            else if ($("#" + start + "").hasClass('down') && direction === 'down'.toString()) {
-//                row = (row + moveSquares > numRows) ? numRows.toString() : (row + moveSquares);
-//            }
-//            end = row.toString() + col.toString();
-////            $("h1").text(Math.floor(end / 10) + "   " + end % 10 + "  " + end);
-//            $("#" + start + "").removeClass('avatar');
-//            $("#" + end + "").addClass('avatar');
-//            $('#grid').text("You are currently in box " + end + " you swiped " + direction + " for " + distance + " pixels");
-//        },
-//        //Default is 75px, set to 0 for demo so any distance triggers swipe
-//        threshold: 0
-//    });
-////    $("table").on("click", function() {
-////        alert("Level 1");
-////        $("#00").text("laser").addClass("laser");
-////        $("#60").text("laser").addClass("laser");
-////        $("#38").text("laser").addClass("laser");
-////        $("#84").addClass("avatar");
-////    });
-
-});
