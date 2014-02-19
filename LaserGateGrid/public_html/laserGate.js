2c681fd9d0acd1d0f53deb2b3d80f90c778fbaf8
Ashley <ablondewell@gmail.com>
2014-02-18 16:37:40 -0800

    Added laser shooting basic functionality. This code is by no means complete as it still shows the transition when the avatar is reset, and has bugs if you reset the avatar or click a laser again too quickly.

--- LaserGateGrid/public_html/laserGate.js
+++ LaserGateGrid/public_html/laserGate.js
@@ -1,13 +1,21 @@
 /* 
+ * @ABroadwell
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-document.write('<h1>Laser Gate</h1><div class="laserGate"><table id="grid" border="0" cellspacing = "0" cellpadding = "0" id="a" align = "center">');
 var numCols = 12;
 var numRows = 12;
-var avatar;
+var avatar = numRows.toString() + Math.floor(numCols / 2).toString();
+var targetX;
+var targetY;
+var avatarX;
+var avatarY;
+var avatarIsPlaced = false;
+
+document.write('<img id="thing" src="http://1.bp.blogspot.com/-VfEiU_WCC0Q/UInN6IcUTDI/AAAAAAAAAH0/HRik5VIq7Y4/s1600/b001.png"><h1 id="test">Laser Gate</h1><div class="laserGate"><table id="grid" border="0" cellspacing = "0" cellpadding = "0" id="a" align = "center">');
+
 for (i = 0; i <= numRows; i++) {
     document.write("<tr id='row'" + i + ">");
     for (j = 0; j <= numCols; j++) {
@@ -44,43 +52,81 @@ for (i = 0; i <= numRows; i++) {
         }
     }
     document.write('</tr></div>');
-}
-;
+};
 
 level1();
 document.write('</table>');
+
+var table = $("#table");
+var cellWidth = document.getElementById("00").offsetWidth / 2;
+var cellHeight = document.getElementById("00").offsetHeight / 2;
 
 function level1() {
     $("#00").text("laser").addClass("laser");
     $("#60").text("laser").addClass("laser");
     $("#3" + numCols + "").text("laser").addClass("laser");
-    $("#" + numRows + "10").text("laser").addClass("laser")
-    avatar = numRows.toString() + Math.floor(numCols/2).toString();
+    $("#" + numRows + "10").text("laser").addClass("laser");
     $("#" + avatar + "").addClass("avatar");
+    var pos = getElementPosition(avatar);
+    avatarX = pos.left + cellWidth;
+    avatarY = pos.top + cellHeight;
 
+    $("#thing").css({
+        left: avatarX,
+        top: avatarY
+    });
+    avatarIsPlaced = true;
 }
+function getElementPosition(id) {
+    var ele = document.getElementById(id);
+    var top = 0;
+    var left = 0;
+
+    while (ele.tagName !== "BODY") {
+        top += ele.offsetTop;
+        left += ele.offsetLeft;
+        ele = ele.offsetParent;
+    }
+    return {top: top, left: left};
+}
+;
+
 $("table").onclick = function getClickPosition(e) {
     var xPosition = e.clientX;
     var yPosition = e.clientY;
     alert(xPosition, yPosition);
 };
+
 var grid = document.getElementById("grid");
 for (i = 0; i <= numRows; i++) {
     for (j = 0; j <= numCols; j++) {
         grid.rows[i].cells[j].onclick = function(e) {
-//            if we want exact x,y locations
-                var xPosition = e.clientX;
-                var yPosition = e.clientY;
-                $("#whereami").text("Current Location: " + xPosition + ", "+ yPosition);
             var currentPosition = $(this).attr("id");
-//                alert(newAvatar);
-            if ($(this).hasClass("laser")) {
-                $("#whereami").text("LASER --- Current Location: " + xPosition + ", "+ yPosition);
+            var position = getElementPosition(currentPosition);
+            var xPosition = position.left + cellWidth;
+            var yPosition = position.top + cellHeight;
+            $("#whereami").text("Current Location: " + xPosition + ", " + yPosition);
+            
+            if ($(this).hasClass("laser") && avatarIsPlaced) {
+                $("#whereami").text("LASER --- Current Location: " + xPosition + ", " + yPosition + " avatar: " + avatarX + "  " + avatarY);
+                laserX = xPosition;
+                laserY = yPosition;
+
+                var theThing = document.querySelector("#thing");
+                theThing.style.left = laserX + "px";
+                theThing.style.top = laserY + "px";
             }
             else if ($(this).hasClass("outer")) {
                 $("#" + avatar + "").removeClass("avatar");
                 $(this).addClass("avatar");
                 avatar = currentPosition;
+                avatarX = xPosition;
+                avatarY = yPosition;
+                $("#thing").css({
+                    left: avatarX,
+                    top: avatarY
+                });
+                avatarIsPlaced = true;
             }
         };
     }
