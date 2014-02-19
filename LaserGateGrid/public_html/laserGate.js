784bf6b572a64fcd07448d2891e6420e86c85451
Ashley <ablondewell@gmail.com>
2014-02-18 17:30:24 -0800

    Working on hiding transitions. Needed code to work on before class

--- LaserGateGrid/public_html/laserGate.js
+++ LaserGateGrid/public_html/laserGate.js
@@ -8,14 +8,12 @@
 var numCols = 12;
 var numRows = 12;
 var avatar = numRows.toString() + "_" + Math.floor(numCols / 2).toString();
-var targetX;
-var targetY;
 var avatarX;
 var avatarY;
 var avatarIsPlaced = false;
 
 document.write('<img id="thing" src="http://1.bp.blogspot.com/-VfEiU_WCC0Q/UInN6IcUTDI/AAAAAAAAAH0/HRik5VIq7Y4/s1600/b001.png"><h1 id="test">Laser Gate</h1><div class="laserGate"><table id="grid" border="0" cellspacing = "0" cellpadding = "0" id="a" align = "center">');
-
+$("#thing").hide();
 for (i = 0; i <= numRows; i++) {
     document.write("<tr class='row" + i + "'>");
     for (j = 0; j <= numCols; j++) {
@@ -42,8 +40,11 @@ for (i = 0; i <= numRows; i++) {
             }
         }
         else {
-            if (i === 0 | i === numRows) {
-                document.write("<td id= '" + i.toString() + "_" + j.toString() + "' class = 'outer'></td>");
+            if (i === 0) {
+                document.write("<td id= '" + i.toString() + "_" + j.toString() + "' class = 'outer top'></td>");
+            }
+            else if (i === numRows) {
+                document.write("<td id= '" + i.toString() + "_" + j.toString() + "' class = 'outer bottom'></td>");
             }
             else {
                 document.write("<td id= '" + i.toString() + "_" + j.toString() + "'></td>");
@@ -52,7 +53,8 @@ for (i = 0; i <= numRows; i++) {
         }
     }
     document.write('</tr></div>');
-};
+}
+;
 
 level1();
 document.write('</table>');
@@ -78,24 +80,18 @@ function level1() {
     avatarIsPlaced = true;
 }
 function getElementPosition(id) {
-    var ele = document.getElementById(id);
+    var element = document.getElementById(id);
     var top = 0;
     var left = 0;
 
-    while (ele.tagName !== "BODY") {
-        top += ele.offsetTop;
-        left += ele.offsetLeft;
-        ele = ele.offsetParent;
+    while (element.tagName !== "BODY") {
+        top += element.offsetTop;
+        left += element.offsetLeft;
+        element = element.offsetParent;
     }
     return {top: top, left: left};
 }
 ;
-
-$("table").onclick = function getClickPosition(e) {
-    var xPosition = e.clientX;
-    var yPosition = e.clientY;
-    alert(xPosition, yPosition);
-};
 
 var grid = document.getElementById("grid");
 for (i = 0; i <= numRows; i++) {
@@ -106,17 +102,22 @@ for (i = 0; i <= numRows; i++) {
             var xPosition = position.left + cellWidth;
             var yPosition = position.top + cellHeight;
             $("#whereami").text("Current Location: " + xPosition + ", " + yPosition);
-            
+
             if ($(this).hasClass("laser") && avatarIsPlaced) {
                 $("#whereami").text("LASER --- Current Location: " + xPosition + ", " + yPosition + " avatar: " + avatarX + "  " + avatarY);
                 laserX = xPosition;
                 laserY = yPosition;
-
+                $("#thing").show();
                 var theThing = document.querySelector("#thing");
                 theThing.style.left = laserX + "px";
                 theThing.style.top = laserY + "px";
+                //Hide thing after it has finished it
+                $("#thing").on('transitionend webkitTransitionEnd', function(e) {
+                    $("#thing").hide();
+                });
             }
             else if ($(this).hasClass("outer")) {
+                $("#thing").hide();
                 $("#" + avatar + "").removeClass("avatar");
                 $(this).addClass("avatar");
                 avatar = currentPosition;
@@ -126,8 +127,43 @@ for (i = 0; i <= numRows; i++) {
                     left: avatarX,
                     top: avatarY
                 });
+                var transitionEnd = whichTransitionEvent();
+                var theThing = document.getElementById("thing");
+                theThing.addEventListener(transitionEnd, $("#thing").show(), false);
+                
+//                
+//                $("#thing").on('transitionend webkitTransitionEnd', function(e) {
+//                    $("#thing").show();
+//                });
+                
+//                $("#thing").show();
+//$("#thing").on('transitionend webkitTransitionEnd', function(e) {
+//                    $("#thing").show();
+//                });
                 avatarIsPlaced = true;
             }
         };
     }
 }
+
+function complete() {
+//    $("#thing").hide();
+}
+
+function whichTransitionEvent() {
+    var t;
+    var el = document.getElementById("thing");
+    var transitions = {
+        'transition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'MozTransition': 'transitionend',
+        'WebkitTransition': 'webkitTransitionEnd'
+    };
+
+    for (t in transitions) {
+        if (el.style[t] !== undefined) {
+            return transitions[t];
+        }
+    }
+}
+
