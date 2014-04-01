1d17f77ea4b207918e0abc4691c6575ec6731bf4
Michael Ortiz <ortiz.d.michael@gmail.com>
2014-04-01 16:41:46 -0700

    added touch event for iPhone touch problem

--- LaserGateGrid/public_html/laserGate.js
+++ LaserGateGrid/public_html/laserGate.js
@@ -25,6 +25,7 @@ if(localStorage.getItem("unlockedLevels")) {
 
 
 //code goes to menu function first
+init();
 startScreen();
 
 
@@ -432,3 +433,38 @@ var levels = {
             ]
 };
 
+function touchHandler(event) //needed for iPhone touch events
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+
+    switch(event.type)
+    {
+       case "touchstart": type = "mousedown"; break;
+       case "touchmove":  type = "mousemove"; break;        
+       case "touchend":   type = "mouseup"; break;
+       default: return;
+    }
+
+    //initMouseEvent(type, canBubble, cancelable, view, clickCount, 
+    //           screenX, screenY, clientX, clientY, ctrlKey, 
+    //           altKey, shiftKey, metaKey, button, relatedTarget);
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                          first.screenX, first.screenY, 
+                          first.clientX, first.clientY, false, 
+                          false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init() 
+{
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);    
+}s
