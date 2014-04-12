8786543fd720a96694750e2bd068af3513ad80e2
Benjamin Magos <bjkmagos@hotmail.com>
2014-04-12 14:35:33 -0700

    Splash Test 4
    Debugging

--- LaserGateGrid/public_html/laserGate.js
+++ LaserGateGrid/public_html/laserGate.js
@@ -31,13 +31,14 @@ if (localStorage.getItem("unlockedLevels")) {
     unlocked = localStorage.getItem("unlockedLevels");
 }; 
 
-//document.addEventListener("deviceready", onDeviceReady, false);
-//
-//function onDeviceReady() {
-//    setTimeout(function () {
-//    navigator.splashscreen.hide();
-//    }, 5000);
-//}
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+    navigator.splashscreen.show();
+    setTimeout(function () {
+    navigator.splashscreen.hide();
+    }, 5000);
+}
 
 //code goes to menu function first
 init();
